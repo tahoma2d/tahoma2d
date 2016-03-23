92d713df0952dc13c7d95d3fd7d0af2d38fae640
kusano <kusano@users.noreply.github.com>
2016-03-24 00:49:38 +0900

    Remove unused members in TFlash

--- toonz/sources/include/tflash.h
+++ toonz/sources/include/tflash.h
@@ -53,9 +53,6 @@ public:
 
 class DVAPI TFlash
 {
-	class Imp;
-	Imp *m_imp;
-
 public:
         static const wstring ConstantLines;
         static const wstring MixedLines;
@@ -102,11 +99,6 @@ public:
 	void endMask() {}
 	void drawHangedObjects() {}
 	void setGlobalScale(const TAffine &aff) {}
-
-private:
-	TFlash();
-	TFlash(const TFlash &);
-	TFlash &operator=(const TFlash &);
 };
 
 #endif
