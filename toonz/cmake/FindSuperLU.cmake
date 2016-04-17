bf80fb1686b6c98842899c182373dc4024601b92
Campbell Barton <ideasman42@gmail.com>
2016-04-18 04:43:41 +1000

    CMake: typo fix

--- toonz/cmake/FindSuperLU.cmake
+++ toonz/cmake/FindSuperLU.cmake
@@ -3,7 +3,7 @@ find_path(SUPERLU_INCLUDE_DIR NAMES slu_Cnames.h HINTS ${THIRDPARTY_LIBS_HINTS} 
 find_library(SUPERLU_LIBRARY NAMES libsuperlu.a libsuperlu_4.1.a HINTS ${THIRDPARTY_LIBS_HINTS} PATH_SUFFIXES superlu43/4.3_1/lib superlu)
 
 message("***** SuperLU Header path:" ${SUPERLU_INCLUDE_DIR})
-message("***** SuperLU Libarary path:" ${SUPERLU_LIBRARY})
+message("***** SuperLU Library path:" ${SUPERLU_LIBRARY})
 
 set(SUPERLU_NAMES ${SUPERLU_NAMES} SuperLU)
 
