326eaa5b7825f3e023234a888912ffd21e1099dd
Campbell Barton <ideasman42@gmail.com>
2016-07-09 10:44:05 +1000

    CMake: use STATUS for messages, spaces indentation

--- toonz/cmake/util_compiler.cmake
+++ toonz/cmake/util_compiler.cmake
@@ -2,32 +2,32 @@ include(CheckCCompilerFlag)
 include(CheckCXXCompilerFlag)
 
 function(ADD_CHECK_C_COMPILER_FLAG
-	_CFLAGS
-	_CACHE_VAR
-	_FLAG
-	)
+    _CFLAGS
+    _CACHE_VAR
+    _FLAG
+    )
 
-	CHECK_C_COMPILER_FLAG("${_FLAG}" "${_CACHE_VAR}")
-	if(${_CACHE_VAR})
-		# message(STATUS "Using CFLAG: ${_FLAG}")
-		set(${_CFLAGS} "${${_CFLAGS}} ${_FLAG}" PARENT_SCOPE)
-	else()
-		message(STATUS "Unsupported CFLAG: ${_FLAG}")
-	endif()
+    CHECK_C_COMPILER_FLAG("${_FLAG}" "${_CACHE_VAR}")
+    if(${_CACHE_VAR})
+        # message(STATUS "Using CFLAG: ${_FLAG}")
+        set(${_CFLAGS} "${${_CFLAGS}} ${_FLAG}" PARENT_SCOPE)
+    else()
+        message(STATUS "Unsupported CFLAG: ${_FLAG}")
+    endif()
 endfunction()
 
 function(ADD_CHECK_CXX_COMPILER_FLAG
-	_CXXFLAGS
-	_CACHE_VAR
-	_FLAG
-	)
+    _CXXFLAGS
+    _CACHE_VAR
+    _FLAG
+    )
 
-	CHECK_CXX_COMPILER_FLAG("${_FLAG}" "${_CACHE_VAR}")
-	if(${_CACHE_VAR})
-		# message(STATUS "Using CXXFLAG: ${_FLAG}")
-		set(${_CXXFLAGS} "${${_CXXFLAGS}} ${_FLAG}" PARENT_SCOPE)
-	else()
-		message(STATUS "Unsupported CXXFLAG: ${_FLAG}")
-	endif()
+    CHECK_CXX_COMPILER_FLAG("${_FLAG}" "${_CACHE_VAR}")
+    if(${_CACHE_VAR})
+        # message(STATUS "Using CXXFLAG: ${_FLAG}")
+        set(${_CXXFLAGS} "${${_CXXFLAGS}} ${_FLAG}" PARENT_SCOPE)
+    else()
+        message(STATUS "Unsupported CXXFLAG: ${_FLAG}")
+    endif()
 endfunction()
 
