d5a8e51d748e5688de82166da4eab1a62602d428
Michał Janiszewski <janisozaur@users.noreply.github.com>
2016-07-14 05:04:33 +0200

    Fix TNZ_MACHINE_CHANNEL_ORDER_MRGB check (#602)

--- toonz/sources/include/tpixel.h
+++ toonz/sources/include/tpixel.h
@@ -53,10 +53,10 @@ public:
   Channel m, b, g, r;
 #elif defined(TNZ_MACHINE_CHANNEL_ORDER_RGBM)
   unsigned char r, g, b, m;
-#elif TNZ_MACHINE_CHANNEL_ORDER_MRGB
+#elif defined(TNZ_MACHINE_CHANNEL_ORDER_MRGB)
   Channel m, r, g, b;
 #else
-  Undefined machine order !!!!
+#error "Undefined machine order !!!!"
 #endif
 
 #ifdef MACOSX
