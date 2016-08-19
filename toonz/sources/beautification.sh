6cf0fab0044aaa4476a18e1de6b999d0bdaad61b
shun-iwasawa <shun-iwasawa@users.noreply.github.com>
2016-08-19 16:24:27 +0900

    fix beautification script (#745)

--- toonz/sources/beautification.sh
+++ toonz/sources/beautification.sh
@@ -1,2 +1,4 @@
 #!/bin/sh
-git diff master --name-only | egrep \\.\(c\|cpp\|h\|hpp\)$ | xargs clang-format -i
+pushd ../../
+git diff master --name-only | egrep \\.\(c\|cpp\|h\|hpp\)$ | xargs clang-format -style=file -i
+popd
