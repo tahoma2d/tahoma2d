16057916f3fd64865a740024f5eb99a8327fa3ff
shun_iwasawa <shun_iwasawa@c.dwango.co.jp>
2016-11-10 22:41:11 +0900

    easy input for name editor

--- toonz/sources/include/toonzqt/stylenameeditor.h
+++ toonz/sources/include/toonzqt/stylenameeditor.h
@@ -4,10 +4,91 @@
 #define STYLENAMEEDITOR_H
 
 #include <QDialog>
+#include <QWidget>
+#include <QStringList>
+#include <QPushButton>
 
 class QLineEdit;
 class QPushButton;
 class TPaletteHandle;
+class QGridLayout;
+class QFrame;
+
+const int WORD_COLUMN_AMOUNT = 3;
+
+//------------------------------------------------------------
+
+class NewWordDialog : public QDialog {
+  Q_OBJECT
+  QLineEdit *m_lineEdit;
+
+public:
+  NewWordDialog(QWidget *parent = Q_NULLPTR);
+  QString getName();
+};
+
+//------------------------------------------------------------
+
+class WordButton : public QPushButton {
+  Q_OBJECT
+public:
+  WordButton(const QString &text, QWidget *parent = Q_NULLPTR);
+
+protected:
+  void contextMenuEvent(QContextMenuEvent *) override;
+
+protected slots:
+  virtual void onClicked();
+  void onRemove();
+
+signals:
+  void clicked(const QString &);
+  void removeWord(const QString &);
+};
+
+//------------------------------------------------------------
+
+class AddWordButton final : public WordButton {
+  Q_OBJECT
+  int m_column;
+
+public:
+  AddWordButton(const int col, QWidget *parent = Q_NULLPTR);
+
+protected slots:
+  void onClicked() override;
+  void contextMenuEvent(QContextMenuEvent *) override{};
+
+signals:
+  void clicked(const int);
+};
+
+//------------------------------------------------------------
+
+class EasyInputArea final : public QWidget {
+  Q_OBJECT
+
+  QStringList m_wordList[WORD_COLUMN_AMOUNT];
+  QGridLayout *m_wordLayout[WORD_COLUMN_AMOUNT];
+  QFrame *m_wordPanel[WORD_COLUMN_AMOUNT];
+
+  void loadList();
+  void saveList();
+  void updatePanelSize(int columnId);
+
+public:
+  EasyInputArea(QWidget *parent = 0);
+  ~EasyInputArea();
+
+protected slots:
+  void addWordButtonClicked(const int);
+  void onRemoveWord(const QString &);
+
+signals:
+  void wordClicked(const QString &);
+};
+
+//------------------------------------------------------------
 
 class StyleNameEditor final : public QDialog  // singleton
 {
@@ -32,6 +113,7 @@ protected slots:
   void onOkPressed();
   void onApplyPressed();
   void onCancelPressed();
+  void onWordClicked(const QString &);
 };
 
 #endif
