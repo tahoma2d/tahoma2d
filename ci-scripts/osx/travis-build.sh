c1e1dc7192e8e92cf10bf999369a5eac328b92aa
shun-iwasawa <shun-iwasawa@users.noreply.github.com>
2017-02-22 22:41:44 +0900

    stop clang-format check in travis (#1041)

--- ci-scripts/osx/travis-build.sh
+++ ci-scripts/osx/travis-build.sh
@@ -1,14 +1,10 @@
 #!/bin/bash
-pushd toonz/sources
-clang-format --version
-git ls-files | egrep \\.\(c\|cpp\|h\|hpp\)$ | xargs clang-format -i && git diff --exit-code || exit 1
-popd
 pushd thirdparty/tiff-4.0.3
 ./configure && make
 popd
 cd toonz && mkdir build && cd build
 cmake ../sources \
-      -DQT_PATH=/usr/local/Cellar/qt@5.5/5.5.1/lib/ \
+      -DQT_PATH=/usr/local/Cellar/qt@5.5/5.5.1_1/lib/ \
       -DTIFF_INCLUDE_DIR=../../thirdparty/tiff-4.0.3/libtiff/ \
       -DSUPERLU_INCLUDE_DIR=../../thirdparty/superlu/SuperLU_4.1/include/
 make
