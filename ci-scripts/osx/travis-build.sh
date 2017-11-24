5a2ff6d27981289760b7d32cb458f5300758a9c8
shun-iwasawa <shun-iwasawa@users.noreply.github.com>
2017-11-24 13:10:45 +0900

    Add add_dependencies Command to CMake for OSX (#1587)
    * fix cmake for osx
    
    * try using make -j 2 for osx travis
    
    * obtain qt5.9.2 from the previous homebrew

--- ci-scripts/osx/travis-build.sh
+++ ci-scripts/osx/travis-build.sh
@@ -7,4 +7,4 @@ cmake ../sources \
       -DQT_PATH=/usr/local/Cellar/qt/5.9.2/lib/ \
       -DTIFF_INCLUDE_DIR=../../thirdparty/tiff-4.0.3/libtiff/ \
       -DSUPERLU_INCLUDE_DIR=../../thirdparty/superlu/SuperLU_4.1/include/
-make
+make -j 2
--- ci-scripts/osx/travis-install.sh
+++ ci-scripts/osx/travis-install.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 brew update
-brew install glew lz4 lzo libusb libmypaint qt
+brew install glew lz4 lzo libusb libmypaint
 brew tap tcr/tcr
 brew install clang-format
+# obtain qt5.9.2 from the previous version of the formula
+curl -O https://raw.githubusercontent.com/Homebrew/homebrew-core/9bc1bdd8d26747cffd7a18c31975f86cd0a97bc3/Formula/qt.rb
+brew install ./qt.rb
