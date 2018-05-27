085f88d57dc9a358646f91d0448a82aa941292a6
Konstantin Dmitriev <ksee.zelgadis@gmail.com>
2018-05-28 06:54:04 +0700

    Add script for updating/adding translation TS files (#1709)
    * Add script for updating/adding translation TS files for particular language.
    
    Usage: update.sh LANGUAGE
    
    With this script it is easy to create new language. I.e. just execute "update.sh portuguese" to add Portuguese language.

---
+++ toonz/sources/translations/update.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+#
+# Usage: update.sh LANGUAGE
+
+BASE_DIR=$(cd `dirname "$0"`; pwd)
+TOONZLANG=$1
+
+[ -d "${BASE_DIR}/${TOONZLANG}" ] || mkdir -p "${BASE_DIR}/${TOONZLANG}"
+cd "${BASE_DIR}/${TOONZLANG}"
+lupdate ../../colorfx/ -ts colorfx.ts
+lupdate ../../common/ -ts tnzcore.ts
+lupdate ../../tnztools/ -ts tnztools.ts
+lupdate ../../toonz/ -ts toonz.ts
+lupdate ../../toonzlib/ -ts toonzlib.ts
+lupdate ../../toonzqt/ -ts toonzqt.ts
+
