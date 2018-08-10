2986f2e399afd94c15e85e7b5f36eeef7eeb9f40
shun-iwasawa <shun-iwasawa@users.noreply.github.com>
2018-08-10 16:07:47 +0900

    continue opening new scene after save levels (#2225)

--- toonz/sources/toonz/iocommand.cpp
+++ toonz/sources/toonz/iocommand.cpp
@@ -1179,8 +1179,6 @@ IoCmd::ConvertingPopup::~ConvertingPopup() {}
 bool IoCmd::saveSceneIfNeeded(QString msg) {
   TApp *app = TApp::instance();
 
-  bool isLevelOrSceneIsDirty = false;
-
   if (app->getCurrentScene()->getDirtyFlag()) {
     QString question;
     question = QObject::tr(
@@ -1201,8 +1199,6 @@ bool IoCmd::saveSceneIfNeeded(QString msg) {
       if (!IoCmd::saveScene()) return false;
     } else if (ret == 3) {
     }
-
-    isLevelOrSceneIsDirty = true;
   }
 
   ToonzScene *scene = app->getCurrentScene()->getScene();
@@ -1232,28 +1228,10 @@ bool IoCmd::saveSceneIfNeeded(QString msg) {
       } else if (ret == 1) {
         // save non scene files
         IoCmd::saveNonSceneFiles();
-        return false;
       } else if (ret == 2) {
-        // quit
+        // do nothing and continue
       }
-
-      isLevelOrSceneIsDirty = true;
     }
-
-    //--- If both the level and scene is clean, then open the quit confirmation
-    // dialog
-    // if (!isLevelOrSceneIsDirty && msg == "Quit") {
-    //  QString question("Are you sure ?");
-    //  int ret =
-    //      DVGui::MsgBox(question, QObject::tr("OK"), QObject::tr("Cancel"),
-    //      0);
-    //  if (ret == 0 || ret == 2) {
-    //    // cancel (or closed message box window)
-    //    return false;
-    //  } else if (ret == 1) {
-    //    // ok
-    //  }
-    //}
 
     RenderingSuspender suspender;
 
