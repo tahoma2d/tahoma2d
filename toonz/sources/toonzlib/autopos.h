65499b52e026f6b2c961a5db835801fe74ed1072
otakuto <otakuto@users.noreply.github.com>
2018-12-17 12:14:31 +0900

    use bool instead of macro bool (#2278)

--- toonz/sources/toonzlib/autopos.h
+++ toonz/sources/toonzlib/autopos.h
@@ -9,14 +9,14 @@
 
 /*---------------------------------------------------------------------------*/
 
-int do_autoalign(const TRasterImageP &image);
+bool do_autoalign(const TRasterImageP &image);
 
 int compute_strip_pixel(CleanupTypes::FDG_INFO *fdg, double dpi);
 
 void convert_dots_mm_to_pixel(CleanupTypes::DOT *dots, int nd, double x_res,
                               double y_res);
 
-int get_image_rotation_and_center(const TRasterP &img, int strip_width,
+bool get_image_rotation_and_center(const TRasterP &img, int strip_width,
                                   CleanupTypes::PEGS_SIDE pegs_side,
                                   double *p_ang, double *cx, double *cy,
                                   CleanupTypes::DOT ref[], int ref_dot);
