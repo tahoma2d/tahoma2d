6dad4e96f4efec853c3d8640220bc58cc16dd34d
manongjohn <manongjohn@users.noreply.github.com>
2018-12-19 20:36:48 -0500

    Fix raster paste crash (#2364)
    * Fix raster paste crash

--- toonz/sources/tnztools/rasterselection.cpp
+++ toonz/sources/tnztools/rasterselection.cpp
@@ -1159,9 +1159,12 @@ void RasterSelection::pasteSelection(const RasterImageData *riData) {
 void RasterSelection::pasteSelection() {
   TTool::Application *app = TTool::getApplication();
   TTool *tool             = app->getCurrentTool()->getTool();
-  TImageP image           = tool->getImage(true);
-  m_currentImage          = image;
-  m_fid                   = tool->getCurrentFid();
+  TImageP image           = tool->touchImage();
+
+  if (!image) return;
+
+  m_currentImage = image;
+  m_fid          = tool->getCurrentFid();
 
   QClipboard *clipboard = QApplication::clipboard();
   const RasterImageData *riData =
