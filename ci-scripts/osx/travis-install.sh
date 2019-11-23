d98db10faea4bfc1b7e42d17ccf6a15946dcdfda
manongjohn <manongjohn@users.noreply.github.com>
2019-11-22 21:30:21 -0500

    Fix macOS brew installed glew issue (#2900)

--- ci-scripts/osx/travis-install.sh
+++ ci-scripts/osx/travis-install.sh
@@ -6,3 +6,11 @@ brew install clang-format
 # from Homebrew 1.6.0 the old formula for obtaining Qt5.9.2 becomes invalid.
 # so we start to use the latest version of Qt. (#1910)
 brew install qt
+# temp workaround to brew installed glew cmake info overriding glew lib detection
+# which causes compiling issues later
+if [ -L /usr/local/lib/cmake/glew ]
+then
+   echo "Symbolic link '/usr/local/lib/cmake/glew' detected. Removing to avoid glew lib detection issue!"
+   ls -l /usr/local/lib/cmake/glew
+   rm /usr/local/lib/cmake/glew
+fi
