4aa8e30ab167bdcaa48d5ddef1ae569bcea7a1d8
MrBlockCat <37186692+MrBlockCat@users.noreply.github.com>
2020-03-18 17:40:13 -0400

    Make icons show up
    Probably
    
    I think this looks right, having looked at the code more closely, though! I'll definitely test this.

--- toonz/sources/toonz/mainwindow.cpp
+++ toonz/sources/toonz/mainwindow.cpp
@@ -1764,19 +1764,19 @@ void MainWindow::defineActions() {
   QAction *openChildAction =
       createMenuXsheetAction(MI_OpenChild, tr("&Open Sub-xsheet"), "");
   openChildAction->setIconText("Open Sub-XSheet");
-  openChildAction->setIcon(createQIconOnOff(":Resources/sub_enter"));
+  openChildAction->setIcon(createQIconOnOff("sub_enter"));
 
   QAction *closeChildAction =
       createMenuXsheetAction(MI_CloseChild, tr("&Close Sub-xsheet"), "");
   closeChildAction->setIconText("Close Sub-XSheet");
-  closeChildAction->setIcon(createQIconOnOff(":Resources/sub_leave"));
+  closeChildAction->setIcon(createQIconOnOff("sub_leave"));
 
   createMenuXsheetAction(MI_ExplodeChild, tr("Explode Sub-xsheet"), "");
 
   QAction *collapseAction =
       createMenuXsheetAction(MI_Collapse, tr("Collapse"), "");
   collapseAction->setIconText("Collapse");
-  collapseAction->setIcon(createQIconOnOff(":Resources/collapse"));
+  collapseAction->setIcon(createQIconOnOff("collapse"));
 
   toggle = createToggle(MI_ToggleEditInPlace, tr("&Toggle Edit In Place"), "",
                         EditInPlaceToggleAction ? 1 : 0, MenuXsheetCommandType);
@@ -1821,7 +1821,7 @@ void MainWindow::defineActions() {
 
   QAction *repeatAction = createMenuCellsAction(MI_Dup, tr("&Repeat..."), "");
   repeatAction->setIconText("Repeat");
-  repeatAction->setIcon(createQIconOnOff(":Resources/repeat_icon"));
+  repeatAction->setIcon(createQIconOnOff("repeat_icon"));
 
   createMenuCellsAction(MI_ResetStep, tr("&Reset Step"), "");
   createMenuCellsAction(MI_IncreaseStep, tr("&Increase Step"), "'");
