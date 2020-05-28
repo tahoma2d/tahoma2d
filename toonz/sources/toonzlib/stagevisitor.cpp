f0e7451df29c46c7d2e4a98a930c2dad0bbf12d1
Jeremy Bullock <jcbullock@gmail.com>
2020-05-28 16:17:17 -0600

    3142 (#24)
    * Fix build with Clang 10
    
    * System depend code deduplication
    
    * Improve multi-layer style picker.
    
    Allows clicking anywhere on a stroke in another layer to pick it -
    not just the center.
    
    Fixes #2843.
    
    * Get actual pixel size instead of hard-coding to 1.0.
    
    Co-authored-by: Rodney <rodney.baker@gmail.com>
    Co-authored-by: Rozhuk Ivan <rozhuk.im@gmail.com>
    Co-authored-by: Martin van Zijl <martin.vanzijl@gmail.com>

--- toonz/sources/toonzlib/stagevisitor.cpp
+++ toonz/sources/toonzlib/stagevisitor.cpp
@@ -250,9 +250,25 @@ void Picker::onImage(const Stage::Player &player) {
     if (r) styleId   = r->getStyle();
     if (styleId != 0)
       picked = true;
-    else if (vi->getNearestStroke(point, w, strokeIndex, dist2) &&
-             dist2 < m_minDist2)
-      picked = true;
+    else if (vi->getNearestStroke(point, w, strokeIndex, dist2)) {
+      // based on TTool::Viewer::doPickGuideStroke
+
+      // m_minDist2 seems to be the pixel size to the power 4, so take the
+      // square root of the square root.
+      // Use abs() just in case m_minDist2 is negative, to avoid math errors.
+      double pixelSize = sqrt(sqrt(abs(m_minDist2)));
+      double maxDist   = 5 * pixelSize;
+      double maxDist2  = maxDist * maxDist;
+      double checkDist = maxDist2 * 4;
+
+      TStroke *stroke = vi->getStroke(strokeIndex);
+      TThickPoint thickPoint = stroke->getThickPoint(w);
+      double thickness = thickPoint.thick;
+      double len = thickness * pixelSize * sqrt(m_viewAff.det());
+      checkDist = std::max(checkDist, (len * len));
+      if (dist2 < checkDist)
+        picked = true;
+    }
   } else if (TRasterImageP ri = img) {
     TRaster32P ras = ri->getRaster();
     if (!ras) return;
