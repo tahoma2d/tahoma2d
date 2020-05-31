dd92b99f0aaa04855c0e2ea127d90597becddaa5
Jeremy Bullock <jcbullock@gmail.com>
2020-05-30 22:23:44 -0600

    Add level button

--- toonz/sources/toonz/layerfooterpanel.h
+++ toonz/sources/toonz/layerfooterpanel.h
@@ -16,51 +16,52 @@ class XsheetViewer;
 
 // Panel showing column footers for layers in timeline mode
 class LayerFooterPanel final : public QWidget {
-	Q_OBJECT
+  Q_OBJECT
 
-	QString m_tooltip;
-	QPoint m_pos;
+  QString m_tooltip;
+  QPoint m_pos;
 
-	QSlider *m_frameZoomSlider;
+  QSlider *m_frameZoomSlider;
 
-	bool isCtrlPressed = false;
-	bool m_zoomInHighlighted = false;
-	bool m_zoomOutHighlighted = false;
+  bool isCtrlPressed         = false;
+  bool m_zoomInHighlighted   = false;
+  bool m_zoomOutHighlighted  = false;
+  bool m_addLevelHighlighted = false;
 
 private:
-	XsheetViewer *m_viewer;
+  XsheetViewer *m_viewer;
 
 public:
 #if QT_VERSION >= 0x050500
-	LayerFooterPanel(XsheetViewer *viewer, QWidget *parent = 0,
-		Qt::WindowFlags flags = 0);
+  LayerFooterPanel(XsheetViewer *viewer, QWidget *parent = 0,
+                   Qt::WindowFlags flags = 0);
 #else
-	LayerFooterPanel(XsheetViewer *viewer, QWidget *parent = 0,
-		Qt::WFlags flags = 0);
+  LayerFooterPanel(XsheetViewer *viewer, QWidget *parent = 0,
+                   Qt::WFlags flags = 0);
 #endif
-	~LayerFooterPanel();
+  ~LayerFooterPanel();
 
-	void showOrHide(const Orientation *o);
+  void showOrHide(const Orientation *o);
 
-	void setZoomSliderValue(int val);
+  void setZoomSliderValue(int val);
 
-	void onControlPressed(bool pressed);
-	const bool isControlPressed();
+  void onControlPressed(bool pressed);
+  const bool isControlPressed();
 
 protected:
-	void paintEvent(QPaintEvent *event) override;
+  void paintEvent(QPaintEvent *event) override;
 
-	void enterEvent(QEvent *) override;
-	void leaveEvent(QEvent *) override;
-	void mousePressEvent(QMouseEvent *event) override;
-	void mouseMoveEvent(QMouseEvent *event) override;
-	bool event(QEvent *event) override;
+  void enterEvent(QEvent *) override;
+  void leaveEvent(QEvent *) override;
+  void mousePressEvent(QMouseEvent *event) override;
+  void mouseMoveEvent(QMouseEvent *event) override;
+  bool event(QEvent *event) override;
 
-	void keyPressEvent(QKeyEvent *event) override { event->ignore(); }
-	void wheelEvent(QWheelEvent *event) override { event->ignore(); }
+  void keyPressEvent(QKeyEvent *event) override { event->ignore(); }
+  void wheelEvent(QWheelEvent *event) override { event->ignore(); }
 
 public slots:
-   void onFrameZoomSliderValueChanged(int val);
+  void onFrameZoomSliderValueChanged(int val);
 };
 #endif
 #pragma once
