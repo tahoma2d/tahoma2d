952984c2751f24bab785e38e5ed08e9a7f0371c7
shun-iwasawa <shun.iwasawa@ghibli.jp>
2020-06-05 12:30:35 +0900

    fix expression reference in sub xsheet

--- toonz/sources/toonzqt/stageobjectsdata.cpp
+++ toonz/sources/toonzqt/stageobjectsdata.cpp
@@ -8,6 +8,8 @@
 
 // TnzBase includes
 #include "tbasefx.h"
+#include "tparamcontainer.h"
+#include "tparamset.h"
 
 // TnzLib includes
 #include "toonz/tstageobject.h"
@@ -114,6 +116,20 @@ bool isColumnSelectionTerminalFx(TFx *fx, TFxSet *terminalFxs,
   }
 
   return false;
+}
+
+//------------------------------------------------------
+
+template <typename ParamCont>
+void setGrammerToParams(const ParamCont *cont,
+                        const TSyntax::Grammar *grammer) {
+  for (int p = 0; p != cont->getParamCount(); ++p) {
+    TParam &param = *cont->getParam(p);
+    if (TDoubleParam *dp = dynamic_cast<TDoubleParam *>(&param))
+      dp->setGrammar(grammer);
+    else if (TParamSet *paramSet = dynamic_cast<TParamSet *>(&param))
+      setGrammerToParams(paramSet, grammer);
+  }
 }
 
 }  // namespace
@@ -1165,6 +1181,23 @@ std::vector<TStageObjectId> StageObjectsData::restoreObjects(
 
       obj->setDagNodePos(oldPos + offset);
     }
+  }
+
+  // reset grammers for all parameters of pasted stage objects and fxs
+  // or they fails to refer to other parameters via expression
+  // if they are pasted in different xsheet
+  TSyntax::Grammar *grammer = xsh->getStageObjectTree()->getGrammar();
+  for (auto id : restoredIds) {
+    TStageObject *obj = xsh->getStageObject(id);
+    for (int c = 0; c != TStageObject::T_ChannelCount; ++c)
+      obj->getParam((TStageObject::Channel)c)->setGrammar(grammer);
+    if (const PlasticSkeletonDeformationP &sd =
+            obj->getPlasticSkeletonDeformation())
+      sd->setGrammar(grammer);
+  }
+  std::map<TFx *, TFx *>::const_iterator it;
+  for (it = fxTable.begin(); it != fxTable.end(); ++it) {
+    setGrammerToParams(it->second->getParams(), grammer);
   }
 
   return restoredIds;
