7e0ba3c713cb3975725b6461de5cc0878fce1c92
Jeremy Bullock <jcbullock@gmail.com>
2020-07-25 22:06:00 -0700

    Redo About Popup (#102)
    * Redo About Popup
    
    * Remove forum link

---
+++ toonz/sources/toonz/aboutpopup.cpp
@@ -0,0 +1,96 @@
+#include "aboutpopup.h"
+#include "tenv.h"
+#include "tsystem.h"
+
+#include <QPushButton>
+#include <QLabel>
+#include <QApplication>
+#include <QDesktopWidget>
+#include <QTextEdit>
+#include <QDesktopServices>
+
+AboutClickableLabel::AboutClickableLabel(QWidget* parent, Qt::WindowFlags f)
+    : QLabel(parent) {
+  setStyleSheet("text-decoration: underline;");
+}
+
+AboutClickableLabel::~AboutClickableLabel() {}
+
+void AboutClickableLabel::mousePressEvent(QMouseEvent* event) {
+  emit clicked();
+}
+
+AboutPopup::AboutPopup(QWidget* parent)
+    : DVGui::Dialog(parent, true, "About Tahoma") {
+  setFixedWidth(300);
+  setFixedHeight(350);
+
+  setWindowTitle(tr("About Tahoma"));
+  setTopMargin(0);
+
+  TFilePath baseLicensePath   = TEnv::getStuffDir() + "doc/LICENSE";
+  TFilePath tahomaLicensePath = baseLicensePath + "LICENSE.txt";
+
+  QVBoxLayout* mainLayout = new QVBoxLayout(this);
+
+  QLabel* logo = new QLabel(this);
+
+  logo->setPixmap(QPixmap::fromImage(QImage(":Resources/startup.png")));
+  mainLayout->addWidget(logo);
+
+  QString name = QString::fromStdString(TEnv::getApplicationFullName());
+  name += "\nBuilt: " __DATE__;
+  mainLayout->addWidget(new QLabel(name));
+
+  QLabel* blankLabel = new QLabel(this);
+  blankLabel->setText(tr(" "));
+  blankLabel->setWordWrap(true);
+
+  mainLayout->addWidget(blankLabel);
+
+  AboutClickableLabel* licenseLink = new AboutClickableLabel(this);
+  licenseLink->setText(tr("Tahoma License"));
+
+  connect(licenseLink, &AboutClickableLabel::clicked, [=]() {
+    if (TSystem::isUNC(tahomaLicensePath))
+      QDesktopServices::openUrl(
+          QUrl(QString::fromStdWString(tahomaLicensePath.getWideString())));
+    else
+      QDesktopServices::openUrl(QUrl::fromLocalFile(
+          QString::fromStdWString(tahomaLicensePath.getWideString())));
+  });
+
+  mainLayout->addWidget(licenseLink);
+
+  AboutClickableLabel* thirdPartyLink = new AboutClickableLabel(this);
+  thirdPartyLink->setText(tr("Third Party Licenses"));
+
+  connect(thirdPartyLink, &AboutClickableLabel::clicked, [=]() {
+    if (TSystem::isUNC(baseLicensePath))
+      QDesktopServices::openUrl(
+          QUrl(QString::fromStdWString(baseLicensePath.getWideString())));
+    else
+      QDesktopServices::openUrl(QUrl::fromLocalFile(
+          QString::fromStdWString(baseLicensePath.getWideString())));
+  });
+
+  mainLayout->addWidget(thirdPartyLink);
+
+  QLabel* ffmpegLabel = new QLabel(this);
+  ffmpegLabel->setText(
+      tr("Tahoma ships with FFmpeg.  \nFFmpeg is licensed under the LGPLv2.1"));
+  mainLayout->addWidget(ffmpegLabel);
+
+  mainLayout->addStretch();
+
+  QFrame* mainFrame = new QFrame(this);
+  mainFrame->setLayout(mainLayout);
+  mainFrame->setFixedWidth(300);
+
+  addWidget(mainFrame);
+
+  QPushButton* button = new QPushButton(tr("Close"), this);
+  button->setDefault(true);
+  addButtonBarWidget(button);
+  connect(button, SIGNAL(clicked()), this, SLOT(accept()));
+}
---
+++ toonz/sources/toonz/aboutpopup.h
@@ -0,0 +1,43 @@
+#pragma once
+
+#ifndef ABOUTPOPUP_H
+#define ABOUTPOPUP_H
+
+#include "toonzqt/dvdialog.h"
+#include <QLabel>
+#include <QWidget>
+#include <Qt>
+
+// forward declaration
+class QPushButton;
+class QLineEdit;
+
+class AboutClickableLabel : public QLabel {
+  Q_OBJECT
+
+public:
+  explicit AboutClickableLabel(QWidget* parent   = Q_NULLPTR,
+                               Qt::WindowFlags f = Qt::WindowFlags());
+  ~AboutClickableLabel();
+
+signals:
+  void clicked();
+
+protected:
+  void mousePressEvent(QMouseEvent* event);
+};
+
+//=============================================================================
+// AboutPopup
+//-----------------------------------------------------------------------------
+
+class AboutPopup final : public DVGui::Dialog {
+  Q_OBJECT
+
+  QPushButton* m_closeBtn;
+
+public:
+  AboutPopup(QWidget* parent);
+};
+
+#endif  // ABOUTPOPUP_H
