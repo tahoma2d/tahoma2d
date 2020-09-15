09be93c8377c156ccbf2f2cb7ad769aa116c34d0
Jeremy Bullock <jcbullock@gmail.com>
2020-09-15 14:27:43 -0600

    Toggle Vector Snapping with Control + Shift (#176)

--- toonz/sources/toonz/statusbar.cpp
+++ toonz/sources/toonz/statusbar.cpp
@@ -131,17 +131,20 @@ void StatusBar::makeMap() {
   m_infoMap.insert(
       {"T_BrushVector", "Brush Tool: Draws in the work area freehand" + spacer +
                             "Shift - Straight Lines" + spacer +
-                            "Hold Alt - Toggle Snapping"});
+                        "Control - Straight Lines Snapped to Angles" + spacer +
+                        "Ctrl + Alt - Add / Remove Vanishing Point" + spacer +
+                        "Alt - Draw to Vanishing Point" + spacer +
+                            "Hold Ctrl + Shift - Toggle Snapping"});
   m_infoMap.insert({"T_BrushSmartRaster",
                     "Brush Tool: Draws in the work area freehand" + spacer +
                         "Shift - Straight Lines" + spacer +
-                        "Control - Vertical and Horizontal Lines" + spacer +
+                        "Control - Straight Lines Snapped to Angles" + spacer +
                         "Ctrl + Alt - Add / Remove Vanishing Point" + spacer +
                         "Alt - Draw to Vanishing Point"});
   m_infoMap.insert(
       {"T_BrushRaster", "Brush Tool: Draws in the work area freehand" + spacer +
                             "Shift - Straight Lines" + spacer +
-                            "Control - Vertical and Horizontal Lines" + spacer +
+                            "Control - Straight Lines Snapped to Angles" + spacer +
                             "Ctrl + Alt - Add / Remove Vanishing Point" +
                             spacer + "Alt - Draw to Vanishing Point"});
   m_infoMap.insert({"T_Geometric", "Geometry Tool: Draws geometric shapes"});
