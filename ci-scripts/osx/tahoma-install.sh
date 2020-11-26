666836c8ef3c0d762d7b283bfdc0f528549be7f5
manongjohn <manongjohn@users.noreply.github.com>
2020-11-26 08:38:11 -0500

    Configure test builds with Github Actions

--- ci-scripts/osx/tahoma-install.sh
+++ ci-scripts/osx/tahoma-install.sh
@@ -2,7 +2,7 @@
 brew update
 # from Homebrew 1.6.0 the old formula for obtaining Qt5.9.2 becomes invalid.
 # so we start to use the latest version of Qt. (#1910)
-brew install qt clang-format glew lz4 lzo libmypaint jpeg-turbo nasm yasm aom dav1d fontconfig freetype gnutls lame libass libbluray libsoxr libvorbis libvpx opencore-amr openh264 openjpeg opus rav1e sdl2 snappy speex tesseract theora webp xvid xz
+brew install boost qt clang-format glew lz4 lzo libmypaint jpeg-turbo nasm yasm aom dav1d fontconfig freetype gnutls lame libass libbluray libsoxr libvorbis libvpx opencore-amr openh264 openjpeg opus rav1e sdl2 snappy speex tesseract theora webp xvid xz
 # delete older qt versions and make sure to have only the latest
 brew cleanup qt
 # temp workaround to brew installed glew cmake info overriding glew lib detection
