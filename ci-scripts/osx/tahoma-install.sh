f22d13b3862746407fdc45acc801d1604c601cac
manongjohn <manongjohn@users.noreply.github.com>
2021-01-26 09:03:23 -0500

    Force remove symlink for brew python install

--- ci-scripts/osx/tahoma-install.sh
+++ ci-scripts/osx/tahoma-install.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 brew update
-# from Homebrew 1.6.0 the old formula for obtaining Qt5.9.2 becomes invalid.
-# so we start to use the latest version of Qt. (#1910)
-brew unlink python@2
+# Remove symlink to bin/2to3 in order for latest python to install
+rm -f '/usr/local/bin/2to3'
 brew install boost qt clang-format glew lz4 lzo libmypaint jpeg-turbo nasm yasm aom dav1d fontconfig freetype gnutls lame libass libbluray libsoxr libvorbis libvpx opencore-amr openh264 openjpeg opus rav1e sdl2 snappy speex tesseract theora webp xvid xz
 # delete older qt versions and make sure to have only the latest
 brew cleanup qt
