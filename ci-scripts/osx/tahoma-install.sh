1f9f576a770241cae39b392a573fe9dd85f949a0
manongjohn <manongjohn@users.noreply.github.com>
2021-01-26 08:17:57 -0500

    Fix macOS build brew link error

--- ci-scripts/osx/tahoma-install.sh
+++ ci-scripts/osx/tahoma-install.sh
@@ -2,6 +2,7 @@
 brew update
 # from Homebrew 1.6.0 the old formula for obtaining Qt5.9.2 becomes invalid.
 # so we start to use the latest version of Qt. (#1910)
+brew unlink python@2
 brew install boost qt clang-format glew lz4 lzo libmypaint jpeg-turbo nasm yasm aom dav1d fontconfig freetype gnutls lame libass libbluray libsoxr libvorbis libvpx opencore-amr openh264 openjpeg opus rav1e sdl2 snappy speex tesseract theora webp xvid xz
 # delete older qt versions and make sure to have only the latest
 brew cleanup qt
