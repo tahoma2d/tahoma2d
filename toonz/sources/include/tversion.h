54db84b734afebcf9f4cc02d84a5e8d1a62909fb
manongjohn <manongjohn@users.noreply.github.com>
2021-04-04 08:53:15 -0400

    Update version to 1.2 (alpha)

--- toonz/sources/include/tversion.h
+++ toonz/sources/include/tversion.h
@@ -18,9 +18,9 @@ public:
 
 private:
   const char *applicationName     = "Tahoma2D";
-  const float applicationVersion  = 1.1;
+  const float applicationVersion  = 1.2;
   const float applicationRevision = 0;
-  const char *applicationNote     = "";
+  const char *applicationNote     = "(alpha)";
 };
 
 std::string ToonzVersion::getAppName(void) {
