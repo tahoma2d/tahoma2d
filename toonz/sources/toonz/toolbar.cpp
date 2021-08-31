opentoonz/opentoonz_nightlies

fix collapsable->collapsible

toonz/sources/toonz/toolbar.cpp
@@ -34,7 +34,7 @@ TEnv::IntVar ShowAllToolsToggle("ShowAllToolsToggle", 1);
 namespace {
 struct {
   const char *toolName;
-  bool collapsable;
+  bool collapsible;
   QAction *action;
 } buttonLayout[] = {{T_Edit, false, 0},        {T_Selection, false, 0},
                     {"Separator_1", false, 0}, {T_Brush, false, 0},
@@ -178,7 +178,7 @@ void Toolbar::updateToolbar() {
         (m_toolbarLevel & LEVELCOLUMN_XSHLEVEL))
       enable = true;
 
-    if (!m_isExpanded && buttonLayout[idx].collapsable) continue;
+    if (!m_isExpanded && buttonLayout[idx].collapsible) continue;
 
     if (!buttonLayout[idx].action) {
       if (isSeparator)

tahoma2d/tahoma2d

toonz/sources/toonz/toolbar.cpp
@@ -163,7 +163,7 @@ void Toolbar::updateToolbar() {
         (m_toolbarLevel & LEVELCOLUMN_XSHLEVEL))
       enable = true;
 
-    if (!m_isExpanded && m_buttonLayout[idx].collapsable) continue;
+    if (!m_isExpanded && m_buttonLayout[idx].collapsible) continue;
 
     if (!m_buttonLayout[idx].action) {
       if (m_buttonLayout[idx].isSeparator)