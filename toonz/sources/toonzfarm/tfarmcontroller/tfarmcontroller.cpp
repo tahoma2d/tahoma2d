manongjohn/OTX

Add FreeBSD (tested) and other BSD systems (not tested) to build and run.
I fail to build TIFF code: it requires access to libtiff internals but it
unavailable with libtiff prom ports. Probably pre build form
thirdparty/tiff-4.0.3 should fix this, but I don't check this.
Tiff code disabled for work but available in options.

I fail with SSE2 too, there is some mess with class initialization:
TRaster32P rout32 = rout; have no idea how to fix it.
If some one want to play with it, then try to build with
CFLAGS+= -DUSE_SSE2 -DDWORD=uint32_t -D_aligned_malloc=aligned_alloc -D_aligned_free=free
on non windows system.

FarmServer::queryHwInfo() - now uses TSystem::***() to collect system info,
to avoid code duplication.

Add pthread as required lib.

toonz/sources/toonzfarm/tfarmcontroller/tfarmcontroller.cpp
@@ -29,7 +29,6 @@ using namespace std;
 #ifndef _WIN32
 #include <sys/param.h>
 #include <unistd.h>
-#include <sys/timeb.h>
 #endif
 
 int inline STRICMP(const QString &a, const QString &b) {
@@ -118,7 +117,11 @@ TFilePath getLocalRoot() {
                          ".app/Contents/Resources/configfarmroot.txt";
 #else
   // set path to something suitable for most linux (Unix?) systems
+#ifdef FREEBSD
+  std::string unixpath = "/usr/local/etc/" + tver.getAppName() + "/opentoonz.conf";
+#else
   std::string unixpath = "/etc/" + tver.getAppName() + "/opentoonz.conf";
+#endif
 #endif
   TFilePath name(unixpath);
   Tifstream is(name);

toonz/sources/toonzfarm/tfarmserver/tfarmserver.cpp
@@ -29,7 +29,6 @@ using namespace TVER;
 #else
 #include <sys/param.h>
 #include <unistd.h>
-#include <sys/timeb.h>
 #endif
 
 //#define REDIRECT_OUPUT
@@ -116,7 +115,11 @@ TFilePath getLocalRoot() {
                          ".app/Contents/Resources/configfarmroot.txt";
 #else
   // set path to something suitable for most linux (Unix?) systems
+#ifdef FREEBSD
+  std::string unixpath = "/usr/local/etc/" + tver.getAppName() + "/opentoonz.conf";
+#else
   std::string unixpath = "/etc/" + tver.getAppName() + "/opentoonz.conf";
+#endif
 #endif
   TFilePath name(unixpath);
   Tifstream is(name);

tahoma2d/tahoma2d

toonz/sources/toonzfarm/tfarmcontroller/tfarmcontroller.cpp
@@ -29,7 +29,6 @@ using namespace std;
 #ifndef _WIN32
 #include <sys/param.h>
 #include <unistd.h>
-#include <sys/timeb.h>
 #endif
 
 int inline STRICMP(const QString &a, const QString &b) {
@@ -119,7 +118,11 @@ TFilePath getLocalRoot() {
       "./" + tver.getAppName() + ".app/Contents/Resources/configfarmroot.txt";
 #else
   // set path to something suitable for most linux (Unix?) systems
+#ifdef FREEBSD
+  std::string unixpath = "/usr/local/etc/" + tver.getAppName() + "/tahoma.conf";
+#else
   std::string unixpath = "/etc/" + tver.getAppName() + "/tahoma.conf";
+#endif
 #endif
   TFilePath name(unixpath);
   Tifstream is(name);

toonz/sources/toonzfarm/tfarmserver/tfarmserver.cpp
@@ -29,7 +29,6 @@ using namespace TVER;
 #else
 #include <sys/param.h>
 #include <unistd.h>
-#include <sys/timeb.h>
 #endif
 
 //#define REDIRECT_OUPUT
@@ -116,7 +115,11 @@ TFilePath getLocalRoot() {
       "./" + tver.getAppName() + ".app/Contents/Resources/configfarmroot.txt";
 #else
   // set path to something suitable for most linux (Unix?) systems
+#ifdef FREEBSD
+  std::string unixpath = "/usr/local/etc/" + tver.getAppName() + "/tahoma.conf";
+#else
   std::string unixpath = "/etc/" + tver.getAppName() + "/tahoma.conf";
+#endif
 #endif
   TFilePath name(unixpath);
   Tifstream is(name);