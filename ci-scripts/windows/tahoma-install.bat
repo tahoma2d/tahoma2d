380608f562050a0a6cad9ae8aa6e0ab17a71cd89
manongjohn <manongjohn@users.noreply.github.com>
2021-10-04 14:58:38 -0400

    Configure Actions to build with Custom Qt 5.15.2

--- ci-scripts/windows/tahoma-install.bat
+++ ci-scripts/windows/tahoma-install.bat
@@ -1,9 +1,15 @@
 choco install opencv --version=4.5.1
 choco install boost-msvc-14.2
 
-REM Install Qt 5.15
-curl -fsSL -o Qt5.15.2_msvc2019_64.zip https://github.com/tahoma2d/qt5/releases/download/v5.15.2/Qt5.15.2_msvc2019_64.zip
-7z x Qt5.15.2_msvc2019_64.zip
-rename Qt5.15.2_msvc2019_64 5.15
 mkdir thirdparty\qt
-move 5.15 thirdparty\qt
+
+REM Install Qt 5.15
+REM curl -fsSL -o Qt5.15.2_msvc2019_64.zip https://github.com/tahoma2d/qt5/releases/download/v5.15.2/Qt5.15.2_msvc2019_64.zip
+REM 7z x Qt5.15.2_msvc2019_64.zip
+REM rename Qt5.15.2_msvc2019_64 5.15
+REM move 5.15 thirdparty\qt
+
+REM Install Custom Qt 5.15.2 with WinTab support
+curl -fsSL -o Qt5.15.2_wintab.zip https://github.com/tahoma2d/qt5/releases/download/v5.15.2/Qt5.15.2_wintab.zip
+7z x Qt5.15.2_wintab.zip
+move Qt5.15.2_wintab\5.15.2_wintab thirdparty\qt
