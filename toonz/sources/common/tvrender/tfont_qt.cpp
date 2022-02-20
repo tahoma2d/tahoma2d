97dfcdf137b4136f945f1635c92481615285d560
manongjohn <manongjohn@users.noreply.github.com>
2022-02-20 11:55:24 -0500

    fix crashes when using type tool (Qt 5.9 fix)

--- toonz/sources/common/tvrender/tfont_qt.cpp
+++ toonz/sources/common/tvrender/tfont_qt.cpp
@@ -179,7 +179,12 @@ TPoint TFont::drawChar(QImage &outImage, TPoint &unused, wchar_t charcode,
   // (21/1/2022) Use this workaround for all platforms as the crash also occured
   // in windows when the display is scaled up.
   if (chars[0].isSpace()) {
+#if QT_VERSION >= 0x051100
     int w = QFontMetrics(m_pimpl->m_font).horizontalAdvance(chars[0]);
+#else
+    int w = raw.averageCharWidth();
+#endif
+
     outImage =
         QImage(w, raw.ascent() + raw.descent(), QImage::Format_Grayscale8);
     outImage.fill(255);
