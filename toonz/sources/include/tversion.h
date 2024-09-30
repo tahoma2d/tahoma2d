75b068216364b5feef234327ecf319d3f7db51b4
manongjohn <19245851+manongjohn@users.noreply.github.com>
2024-09-30 06:47:26 -0400

    v1.4.5

--- toonz/sources/include/tversion.h
+++ toonz/sources/include/tversion.h
@@ -19,7 +19,7 @@ public:
 private:
   const char *applicationName     = "Tahoma2D";
   const float applicationVersion  = 1.4f;
-  const float applicationRevision = 4;
+  const float applicationRevision = 5;
   const char *applicationNote     = "";
 };
 
