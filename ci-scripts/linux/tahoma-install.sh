6df01e527c4ee87929570f8de027ca80bff9c386
manongjohn <19245851+manongjohn@users.noreply.github.com>
2026-06-16 09:44:19 -0400

    Fix AppImage Creation
    Commented out installation steps for dependencies and building process.

--- ci-scripts/linux/tahoma-install.sh
+++ ci-scripts/linux/tahoma-install.sh
@@ -19,20 +19,20 @@ pip3 install numpy
 cd ..
 
 ##### This is temporary because linuxdeploy won't work on Ubuntu 22.04 until May 1st 2025
-
-sudo apt-get -y install git g++ libgl1-mesa-dev
-git clone https://github.com/tahoma2d/linuxdeployqt.git
-# Then build in Qt Creator, or use
-export PATH=$(readlink -f /tmp/.mount_QtCreator-*-x86_64/*/gcc_64/bin/):$PATH
-cd linuxdeployqt
-git checkout tahoma2d_version
-qmake
-make
-sudo make install
-
-#wget https://nixos.org/releases/patchelf/patchelf-0.9/patchelf-0.9.tar.bz2
-#tar xf patchelf-0.9.tar.bz2
-#( cd patchelf-0.9/ && ./configure  && make && sudo make install )
-
-sudo wget -c "https://github.com/AppImage/AppImageKit/releases/download/continuous/appimagetool-x86_64.AppImage" -O /usr/local/bin/appimagetool
-sudo chmod a+x /usr/local/bin/appimagetool
+#
+#sudo apt-get -y install git g++ libgl1-mesa-dev
+#git clone https://github.com/tahoma2d/linuxdeployqt.git
+## Then build in Qt Creator, or use
+#export PATH=$(readlink -f /tmp/.mount_QtCreator-*-x86_64/*/gcc_64/bin/):$PATH
+#cd linuxdeployqt
+#git checkout tahoma2d_version
+#qmake
+#make
+#sudo make install
+#
+##wget https://nixos.org/releases/patchelf/patchelf-0.9/patchelf-0.9.tar.bz2
+##tar xf patchelf-0.9.tar.bz2
+##( cd patchelf-0.9/ && ./configure  && make && sudo make install )
+#
+#sudo wget -c "https://github.com/AppImage/AppImageKit/releases/download/continuous/appimagetool-x86_64.AppImage" -O /usr/local/bin/appimagetool
+#sudo chmod a+x /usr/local/bin/appimagetool
