d387467290f7d7390c6b891a5a7be8e034f5dc03
manongjohn <19245851+manongjohn@users.noreply.github.com>
2026-08-14 08:22:24 -0400

    Fix macOS libghoto2 biuld

--- ci-scripts/osx/tahoma-buildlibgphoto2.sh
+++ ci-scripts/osx/tahoma-buildlibgphoto2.sh
@@ -8,13 +8,27 @@ cd libgphoto2_src
 
 git checkout tahoma2d-version-2.5.34
 
+export GETTEXT_PATH=`brew --prefix gettext`
+
 echo ">>> Configuring libgphoto2"
 autoreconf --install --symlink
 
-./configure --prefix=/usr/local
+./configure --prefix=/usr/local \
+  CPPFLAGS="-I$GETTEXT_PATH/include" \
+  LDFLAGS="-L$GETTEXT_PATH/lib -lintl"
+
+if [ $? != 0 ]
+then
+   exit 1
+fi
 
 echo ">>> Making libgphoto2"
 make
+
+if [ $? != 0 ]
+then
+   exit 1
+fi
 
 echo ">>> Installing libgphoto2"
 sudo make install
